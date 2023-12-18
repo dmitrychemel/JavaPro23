164d43eaecd5d244d52b8dcf393ed3bd3aede9a2
Dmitry Chemel <chemel80@gmail.com>
2023-12-18 15:43:30 +0100

    add homework_13_12_2023 Ñadditionally Task8 and Task9

---
+++ src/homework_13_12_2023/Task8.java
@@ -0,0 +1,33 @@
+package homework_13_12_2023;
+
+import java.util.*;
+
+public class Task8 {
+    //    List<String> перебрать и собрать все возможные комбинации стрингов без повторений
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> stringCombination(List<String> list) {
+        List<String> combinationList = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+        Queue<String> queue = new LinkedList<>(list);
+        stack.addAll(list);
+
+        while (stack.isEmpty()) {
+            while(queue.isEmpty()) {
+                sb.append(stack.peek()).append(", ").append(queue.peek());
+                if(!combinationList.contains(sb.toString())) {
+                    combinationList.add(sb.toString());
+                    stack.pop();
+                    queue.poll();
+                }
+                sb.delete(0,sb.length());
+            }
+        }
+
+        return combinationList;
+    }
+}
